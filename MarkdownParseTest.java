a858cbaa9dccbb74269abe45695bec220a3d29d0
Yuxin Guo <emily20021030@gmail.com>
2022-04-23 20:50:03 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,15 +10,9 @@ import java.util.ArrayList;
 public class MarkdownParseTest {
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-
-    @Test
     public void testMarkdownTest() throws IOException {
 
-        Path fileName = Path.of("test-file1.md");
+        Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<>();
