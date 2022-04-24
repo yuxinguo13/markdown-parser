13554869fa645590eb6d25cfccb99d94dcc32333
Yuxin Guo <emily20021030@gmail.com>
2022-04-24 00:27:27 -0700

    update for test-file5

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,12 +12,12 @@ public class MarkdownParseTest {
     @Test
     public void testMarkdownTest() throws IOException {
 
-        Path fileName = Path.of("test-file3.md");
+        Path fileName = Path.of("test-file5.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<>();
-        result.add("https://github.com/yuxinguo13");
-        result.add("https://ucsd.edu");
+        result.add("stuff");
+        result.add("page.com");
         assertEquals(result, links);
     }
 
