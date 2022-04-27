f487cff1a8675331f1d68cb43f414ddb712f8a53
Yuxin Guo <emily20021030@gmail.com>
2022-04-26 17:01:30 -0700

    change the way to read file, through scanner

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,34 +1,47 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MarkdownParse {
 
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            toReturn.add(markdown.substring(openBracket + 1, closeBracket));
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+    public static ArrayList<String> getLinks(Scanner myReader) {
+        boolean next = true;
+        String line = myReader.nextLine();
+        ArrayList<String> result = new ArrayList<>();
+        
+        while ((line != "") && next) {
+            int currentIndex = 0;
+            while(currentIndex < line.length()) {
+                int openBracket = line.indexOf("[", currentIndex);
+                int closeBracket = line.indexOf("]", openBracket);
+                int openParen = line.indexOf("(", closeBracket);
+                int closeParen = line.indexOf(")", openParen);
+                result.add(line.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            } 
+            next = myReader.hasNext();
+            if (!next) {
+                break;
+            }
+            line = myReader.nextLine();
         }
-
-        return toReturn;
+        return result;
     }
 
 
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        String fileName = args[0];
+        File file = new File(fileName);
+        Scanner myReader = new Scanner(file);
+        ArrayList<String> result = getLinks(myReader);
+        
+        for (int i =0; i < 2; i ++) {
+            System.out.println(result.get(i));
+        }
     }
 }
